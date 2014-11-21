80e56977d3a13907f92ec9d409f42ec6e5b287e1
Brian Runnells <brian.runnells@markit.com>
2014-11-21 16:49:01 -0700

    Embed WIP

--- app/models/post.js
+++ app/models/post.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import ajax from 'ic-ajax';
 
 export default DS.Model.extend({
   slug: DS.attr('string'),
@@ -6,8 +7,26 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   title_plain: DS.attr('string'),
   date: DS.attr('date'),
+  embed_url: DS.attr('string'),  
 
   climbers: DS.hasMany('climber'),
   areas: DS.hasMany('area'),
-  categories: DS.hasMany('category')
+  categories: DS.hasMany('category'),
+
+  embed_html: function() {
+  	var embed = this.get('embed_url');
+  	if (embed.indexOf('vimeo') === -1) {
+			return embed;
+		}
+
+		//var url = endpoint + '?url=' + encodeURIComponent(input) + '&callback=' + callback + '&width=640';
+		var url = 'http://vimeo.com/api/oembed.json?url=' + embed;
+		return "<strong>foo</strong>";
+		
+		ajax(url).then(function(result) {
+			debugger
+			// return Ember.Handlebars.SafeString(result.html);
+			return result.html;
+		});
+  }.property('embed_url')
 });
