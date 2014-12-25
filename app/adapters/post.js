e2a43d908d9a5044c7dc3fd0c6d1eacbb7109c98
Brian Runnells <brian.runnells@gmail.com>
2014-12-25 13:45:27 -0700

    Fix tabs

--- app/adapters/post.js
+++ app/adapters/post.js
@@ -2,6 +2,6 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
 	pathForType: function() {
-    	return 'get_recent_posts/?post_type=videos';
-  	}
+		return 'get_recent_posts/?post_type=videos';
+	}
 });
