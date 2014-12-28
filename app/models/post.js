00330537d3f912f67b5ecf5dc6ac87c6215418d7
Brian Runnells <brian.runnells@gmail.com>
2014-12-27 18:53:06 -0700

    Add prev/next url fields to post model

--- app/models/post.js
+++ app/models/post.js
@@ -7,6 +7,8 @@ export default DS.Model.extend({
   title_plain: DS.attr('string'),
   date: DS.attr('date'),
   embed_url: DS.attr('string'),
+  previous_url: DS.attr('string'),
+  next_url: DS.attr('string'),
 
   climbers: DS.hasMany('climber'),
   areas: DS.hasMany('area'),
