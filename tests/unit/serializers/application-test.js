9e47479b067a6b651eda9e1a23153d5131827de5
Brian Runnells <brian.runnells@gmail.com>
2015-01-02 10:20:20 -0700

    Add Video route

--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -3,13 +3,13 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('adapter:post', 'PostAdapter', {
+moduleFor('serializer:application', 'ApplicationSerializer', {
   // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
 });
 
 // Replace this with your real tests.
 test('it exists', function() {
-  var adapter = this.subject();
-  ok(adapter);
+  var serializer = this.subject();
+  ok(serializer);
 });
