deb5ef6121d0f19acff27fa07db56c7ac2de2ea0
Brian Runnells <brian.runnells@gmail.com>
2015-01-03 13:25:11 -0700

    Add bootswatch

--- Brocfile.js
+++ Brocfile.js
@@ -18,6 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootswatch/paper/bootstrap.css');
 
 module.exports = app.toTree();
